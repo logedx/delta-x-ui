0c68b2a01957d4964382f7a8e0b1f54603829a6c
leiddx <leiddx@hotmail.com>
2025-03-22 19:53:17 +0800

    ✨ feat(components): virtual host

--- src/components/icon.ts
+++ src/components/icon.ts
@@ -11,6 +11,12 @@ export type TProperty = {
 
 Component(
 	{
+		options: {
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			virtualHost: true,
+
+		},
+
 		properties: {
 			src: { type: String, value: '' },
 			size: { type: String, value: 'var(--u-06-s)' },
