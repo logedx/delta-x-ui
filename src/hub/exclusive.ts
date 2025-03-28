c5c64649c17e2cb1c7897d03e08a72786c3357e7
leiddx <leiddx@hotmail.com>
2025-03-28 17:51:31 +0800

    ✨ feat(storage & hub): new methods

--- src/hub/exclusive.ts
+++ src/hub/exclusive.ts
@@ -1,6 +1,7 @@
 import * as container from '../lib/container.js'
 
 import * as user_storage from '../storage/user.js'
+import * as scope_storage from '../storage/scope.js'
 
 
 
@@ -22,4 +23,29 @@ export const user = new container.Exclusive()
 
 		user_storage.update,
 
+	)
+
+
+export const scope = new container.Exclusive()
+	.on(
+		'create',
+
+		scope_storage.create,
+	)
+	.on(
+		'retrieve',
+
+		scope_storage.retrieve,
+
+	)
+	.on(
+		'update',
+
+		scope_storage.update,
+
+	)
+	.on(
+		'delete',
+
+		scope_storage.delete_,
 	)
