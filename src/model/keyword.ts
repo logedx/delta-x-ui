1c16624e772ed7503cbc4fcf26de3ebc628c40c7
leiddx <leiddx@hotmail.com>
2025-04-28 17:01:15 +0800

    ✨ feat(model & storage): new fields & new methods

---
+++ src/model/keyword.ts
@@ -0,0 +1,32 @@
+/**
+ * 关键字模型
+ */
+import * as model from '../lib/model.js'
+
+import * as weapp_model from './weapp.js'
+
+
+
+
+export type TRawDocType = model.TRawDocType<
+	{
+		weapp: string
+		model: string
+		name: string
+		value: string
+		letter: string
+
+	}
+
+>
+
+export type TRawDocKeyword = 'value'
+
+export type TPopulatePaths = {
+	weapp: weapp_model.THydratedDocumentType
+
+}
+
+export type TVirtuals = object
+
+export type THydratedDocumentType = model.HydratedDocument<TRawDocType, TVirtuals>
---
+++ src/storage/keyword.ts
@@ -0,0 +1,61 @@
+import http, { HttpTaskUnpackingResult } from '../index.js'
+
+import * as container from '../lib/container.js'
+
+import * as keyword_model from '../model/keyword.js'
+
+
+
+
+export async function create(
+	params: {
+		model: string
+		name: string
+		value: string
+
+	},
+
+): HttpTaskUnpackingResult<void> {
+	let h = http.post(
+		'/keyword', params,
+
+	)
+
+	await h.finish
+
+}
+
+export async function retrieve_pagination(
+	params: container.PaginationParams<
+			{ model: string, name: string }
+
+		>,
+
+): HttpTaskUnpackingResult<
+	Array<keyword_model.THydratedDocumentType>
+
+> {
+	let h = http.get<
+		Array<keyword_model.THydratedDocumentType>
+
+	>(
+		'/keyword', params,
+
+	)
+
+	let doc = await h.collect()
+
+	return doc.data
+
+}
+
+
+export async function delete_(_id: string): HttpTaskUnpackingResult<void> {
+	let h = http.delete(
+		`/user/${_id}`,
+
+	)
+
+	await h.finish
+
+}
