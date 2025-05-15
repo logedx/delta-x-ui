78e0860cda1fab41e694e9998df9cdc462d5592f
leiddx <leiddx@hotmail.com>
2025-05-15 16:55:46 +0800

    ✨ feat(components): dynamic navigation bar color

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -26,6 +26,45 @@ Component(
 			home: { type: String, value: '' },
 			sink: { type: Boolean, value: false },
 			loading: { type: Boolean, value: false },
+			contrast: { type: Boolean, value: false },
+
+		},
+
+		lifetimes: {
+			attached() {
+				this.set_navigation_bar_color()
+
+			},
+
+		},
+
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		pageLifetimes: {
+			show() {
+				this.set_navigation_bar_color()
+
+			},
+
+		},
+
+		methods: {
+			set_navigation_bar_color(): void {
+				let { contrast } = this.data
+
+				// eslint-disable-next-line @typescript-eslint/naming-convention
+				let option = { frontColor: '#ffffff', backgroundColor: '#000000' }
+
+				if (contrast) {
+					option.frontColor = '#000000'
+					option.backgroundColor = '#ffffff'
+
+				}
+
+				// eslint-disable-next-line @typescript-eslint/no-floating-promises
+				wx.setNavigationBarColor(option)
+
+
+			},
 
 		},
 
